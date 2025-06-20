69c296b09aaace5de42cd0ddbd394717e56136f8
Shchuda <sheel2002lee@gmail.com>
2025-06-20 13:40:08 +0300

    bubble and selection sort done

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+print(bubble_sort([5, 4, 3, 2, 1]))
